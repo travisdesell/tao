f4e7009d2f76e34580017eb0b416f04fcb4c7d57
Travis Desell <travis.desell@gmail.com>
2012-07-04 15:09:02 -0500

    Using old matrix library

--- synchronous_algorithms/hessian.cxx
+++ synchronous_algorithms/hessian.cxx
@@ -2,6 +2,7 @@
 #include <vector>
 
 #include "hessian.hxx"
+#include "matrix.hxx"
 
 #include <boost/numeric/ublas/matrix.hpp>
 #include <boost/numeric/ublas/lu.hpp>
@@ -63,6 +64,7 @@ void get_hessian(double (*objective_function)(const std::vector<double> &), cons
 /** Matrix inversion routine.
  *  Uses lu_factorize and lu_substitute in uBLAS to invert a matrix.
  */
+/*
 template<class T>
 bool InvertMatrix(const matrix<T>& input, matrix<T>& inverse) {
     typedef permutation_matrix<std::size_t> pmatrix;
@@ -86,9 +88,10 @@ bool InvertMatrix(const matrix<T>& input, matrix<T>& inverse) {
     return true;
 }
 
-
 void randomized_hessian(const vector< vector<double> > &points, const vector<double> &fitness, const vector<double> &center, vector< vector< double> > &hessian, vector<double> &gradient) {
     uint32_t number_parameters = center.size();
+
+    X LENGTH IS NOT RIGHT
     uint32_t x_length = 1 + number_parameters + number_parameters + (number_parameters * number_parameters);
 
     matrix<double> X(points.size(), x_length);     //create a matrix: number of points by x_length
@@ -134,43 +137,32 @@ void randomized_hessian(const vector< vector<double> > &points, const vector<dou
         }
     }
 }
+*/
 
 /*
- *  OLD C VERSION AS FOLLOWS (maybe want to test)
- *
-void randomized_hessian(double** actual_points, double* center, double* fitness, int number_points, int number_parameters, double*** hessian, double** gradient) {
-    double** Y;
-    double** X;
-    double** X2;
-    double** X3;
-    double** X_transpose;
-    double** X_inverse;
-    double** W;
-    double** points;
-    int x_len, i, j, k, current;
-
-    points = (double**)malloc(sizeof(double*) * number_points);
-    for (i = 0; i < number_points; i++) {
-        points[i] = (double*)malloc(sizeof(double) * number_parameters);
-        for (j = 0; j < number_parameters; j++) {
-            points[i][j] = center[j] - actual_points[i][j];
+ *  OLD C VERSION AS FOLLOWS.
+ *  TODO: compare vs boost version above
+ */
+void randomized_hessian(const vector< vector<double> > &actual_points, const vector<double> &center, const vector<double> &fitness, vector< vector<double> > &hessian, vector<double> &gradient) {
+    vector< vector<double> > points( actual_points );
+    for (uint32_t i = 0; i < points.size(); i++) {
+        for (uint32_t j = 0; j < points[i].size(); j++) {
+            points[i][j] = center[j] - points[i][j];
         }
     }
 
-
     /********
      *	X = [1, x1, ... xn, 0.5*x1^2, ... 0.5*xn^2, x1*x2, ..., x1*xn, x2*x3, ..., x2*xn, ...]
-     ********/ /*
-    x_len = 1 + number_parameters + number_parameters;
+     ********/
+    uint32_t x_len = 1 + number_parameters + number_parameters;
     for (i = number_parameters - 1; i > 0; i--) x_len += i;
 
+    vector< vector<double> > Y(points.size(), vector<double>(1, 0.0));
+    vector< vector<double> > X(points.size(), vector<double>(x_len, 0.0));
 
-    Y = (double**)malloc(sizeof(double*) * number_points);
-    X = (double**)malloc(sizeof(double) * number_points);
     for (i = 0; i < number_points; i++) {
-        Y[i] = (double*)malloc(sizeof(double) * 1);
         Y[i][0] = fitness[i];
-        X[i] = (double*)malloc(sizeof(double) * x_len);
+
         X[i][0] = 1;
         for (j = 0; j < number_parameters; j++) {
             X[i][1+j] = points[i][j];
@@ -185,58 +177,23 @@ void randomized_hessian(double** actual_points, double* center, double* fitness,
         }
     }
 
-    matrix_transpose__alloc(X, number_points, x_len, &X_transpose);
-    matrix_multiply__alloc(X_transpose, x_len, number_points, X, number_points, x_len, &X2);
-    matrix_invert__alloc(X2, x_len, x_len, &X_inverse);
-    matrix_multiply__alloc(X_inverse, x_len, x_len, X_transpose, x_len, number_points, &X3);
-    matrix_multiply__alloc(X3, x_len, number_points, Y, number_points, 1, &W);
-
-
-    (*gradient) = (double*)malloc(sizeof(double) * number_parameters);
-    (*hessian) = (double**)malloc(sizeof(double*) * number_parameters);
-    for (i = 0; i < number_parameters; i++) (*hessian)[i] = (double*)malloc(sizeof(double) * number_parameters);
+    matrix_transpose(X, X_transpose);
+    matrix_multiply(X_transpose, X, X2);
+    matrix_invert(X2, X_inverse);
+    matrix_multiply(X_inverse, X_transpose, X3);
+    matrix_multiply(X3, Y, W);
 
     for (i = 0; i < number_parameters; i++) {
-        (*gradient)[i] = W[1+i][0];
-        (*hessian)[i][i] = W[1 + number_parameters + i][0];
+        gradient[i] = W[1+i][0];
+        hessian[i][i] = W[1 + number_parameters + i][0];
 
         current = 0;
         for (j = i; j < number_parameters; j++) {
             for (k = j+1; k < number_parameters; k++) {
-                (*hessian)[j][k] = W[1 + number_parameters + number_parameters + current][0];
-                (*hessian)[k][j] = W[1 + number_parameters + number_parameters + current][0];
+                hessian[j][k] = W[1 + number_parameters + number_parameters + current][0];
+                hessian[k][j] = W[1 + number_parameters + number_parameters + current][0];
                 current++;
             }
         }
     }
-
-    /********
-     *	Y = number_points by 1
-     *	X = number_points by x_len
-     *	X2 = x_len by x_len
-     *	X3 = x_len by number_points
-     *	X_transpose = x_len by number_points
-     *	X_inverse = x_len by x_len
-     *	W = x_len by 1
-     ********/ /*
-    for (i = 0; i < number_points; i++) {
-        free(Y[i]);
-        free(X[i]);
-        free(points[i]);
-    }
-    for (i = 0; i < x_len; i++) {
-        free(X2[i]);
-        free(X3[i]);
-        free(X_transpose[i]);
-        free(X_inverse[i]);
-        free(W[i]);
-    }
-    free(points);
-    free(Y);
-    free(X);
-    free(X2);
-    free(X3);
-    free(X_transpose);
-    free(X_inverse);
-    free(W);
-} */
+}
--- synchronous_algorithms/hessian.hxx
+++ synchronous_algorithms/hessian.hxx
@@ -7,6 +7,6 @@ using std::vector;
 
 void get_hessian(double (*objective_function)(const std::vector<double> &), const vector<double> &point, const vector<double> &step, vector< vector<double> > &hessian);
 
-void randomized_hessian(const vector< vector<double> > &points, const vector<double> &fitness, const vector<double> &center, vector< vector< double> > &hessian, vector<double> &gradient);
+void randomized_hessian(const vector< vector<double> > &actual_points, const vector<double> &center, const vector<double> &fitness, vector< vector<double> > &hessian, vector<double> &gradient);
 
 #endif
